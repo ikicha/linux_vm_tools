9ac49a41bb72df62c51852fe24a854c86c82e08f
Jeongik Cha <jeongik@google.com>
2024-09-02 15:31:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,20 +9,20 @@ wget -q https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.aarch64 -O 
 echo Customize the image...
 virt-customize --commands-from-file <(sed "s|/tmp|$tempdir|g" commands) -a ${tempdir}/debian.img
 
-asset_dir=../assets/linux
-mkdir -p ${asset_dir}
+output_dir=out/linux
+mkdir -p ${output_dir}
 
 echo Copy files...
 
 pushd ${tempdir} > /dev/null
 tar czvS -f images.tar.gz debian.img
 popd > /dev/null
-mv ${tempdir}/images.tar.gz ${asset_dir}/images.tar.gz
-cp vm_config.json ${asset_dir}
+mv ${tempdir}/images.tar.gz ${output_dir}/images.tar.gz
+cp vm_config.json ${output_dir}
 
 echo Calculating hash...
-hash=$(cat ${asset_dir}/images.tar.gz ${asset_dir}/vm_config.json | sha1sum | cut -d' ' -f 1)
-echo ${hash} > ${asset_dir}/hash
+hash=$(cat ${output_dir}/images.tar.gz ${output_dir}/vm_config.json | sha1sum | cut -d' ' -f 1)
+echo ${hash} > ${output_dir}/hash
 
 popd > /dev/null
 echo Cleaning up...
