1e87bc3c8ace9f871ac600d77a5133ba170afa1f
Jeongik Cha <jeongik@google.com>
2024-09-11 21:35:50 +0900

    Update setup_x86_64.sh

--- setup_x86_64.sh
+++ setup_x86_64.sh
@@ -15,10 +15,13 @@ mkdir -p ${output_dir}
 echo Copy files...
 
 pushd ${tempdir} > /dev/null
-tar czvS -f images.tar.gz debian.img
+virt-get-kernel -a debian.img
+mv vmlinuz* vmlinuz
+mv initrd.img* initrd.img
+tar czvS -f images.tar.gz debian.img vmlinuz initrd.img
 popd > /dev/null
 mv ${tempdir}/images.tar.gz ${output_dir}/images.tar.gz
-cp vm_config.json ${output_dir}
+cp vm_config_x86_64.json ${output_dir}/vm_config.json
 
 echo Calculating hash...
 hash=$(cat ${output_dir}/images.tar.gz ${output_dir}/vm_config.json | sha1sum | cut -d' ' -f 1)
